e82fe817d91b2fc22a96c2f7feb4bb4ed85a0755
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-21 10:11:51 -0300

    feat: four ways to clone an object

---
+++ src/map-filter-reduce/clone-object.ts
@@ -0,0 +1,15 @@
+const input = {a:1, b:2}
+
+const clone = Object.prototype.constructor(input)
+const clone2 = Object.assign({}, input)
+const clone3 = {...input}
+const clone4 = JSON.parse(JSON.stringify(input))
+
+console.log(clone)
+console.log(clone === input)
+console.log(clone2 === input)
+console.log(clone3 === input)
+console.log(clone4 === input)
+
+input.a = 3
+console.log(clone)
