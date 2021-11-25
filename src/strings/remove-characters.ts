cd2305977b6cc8f13cc64c412491c1b7f4b4b645
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-25 09:07:28 -0300

    refactor: reimplementing exercise with regexp

--- src/strings/remove-characters.ts
+++ src/strings/remove-characters.ts
@@ -1,9 +1,13 @@
-// const sr =new RegExp(chars.join('|'), 'gi',)
-
 type RemoveChars = (str: string, chars: string[]) => string
 
-export const removeChars:RemoveChars = (str, chars) =>
-  str
-    .split('')
-    .filter( char => {if(chars.indexOf(char) === -1) return char})
-    .join('')
+export const removeChars:RemoveChars = (str, chars) =>{
+  const regexStr = chars.join('|')
+  const regexp = new RegExp(regexStr, 'gi')
+  const newWord = str.replace(regexp, '')
+  console.log(newWord)
+  return newWord
+}
+  // str
+  //   .split('')
+  //   .filter( char => {if(chars.indexOf(char) === -1) return char})
+  //   .join('')
