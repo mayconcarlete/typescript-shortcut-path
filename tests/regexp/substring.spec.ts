dcdf2250aa5c36c00d90a6542a9cd18a92977ea9
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-27 09:05:52 -0300

    test: ensure Substring return true when a substring matches with str

---
+++ tests/regexp/substring.spec.ts
@@ -0,0 +1,23 @@
+interface Validate {
+  validate(str: string, substring: string): boolean
+}
+
+class VerifyStr implements Validate{
+  validate(str: string, substring: string): boolean {
+    const regexp = new RegExp(substring, 'gi')
+    return regexp.test(str)
+  }
+}
+
+describe('Substring', () => {
+  it('should verify if a given string is a substring of another', () => {
+    const str = 'Maycon Carlete'
+    const substring = 'carlete'
+    const sut = new VerifyStr()
+    // arrange
+
+    const result = sut.validate(str, substring) // act
+
+    expect(result).toBeTruthy()
+  })
+})
