268d9e12d43d14da5be2cbc64ae280dc31a98c5d
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-29 08:26:08 -0300

    feat: ensure EmailValidator returns false if domain email name ends with period, udnerscore or hyphen

--- tests/regexp/email-validator.spec.ts
+++ tests/regexp/email-validator.spec.ts
@@ -4,7 +4,7 @@ interface Validate {
 
 class EmailValidator implements Validate{
   validate(email: string): boolean {
-    const pattern = /^[a-zA-Z0-9][\w\.-]{0,63}@mail\.com$/
+    const pattern = /^[a-zA-Z0-9][\w\.-]{0,62}[a-zA-Z]@mail\.com$/
     const regexp = new RegExp(pattern,'gi')
     return regexp.test(email)
   }
@@ -54,16 +54,22 @@ describe('EmailValidator', () => {
   it('should return false if email starts with allowed special character', () => {
     const email1 = '-invalid@mail.com'
     const email2 = '_invalid@mail.com'
+    const email3 = 'invalid.@mail.com'
+    const email4 = 'invalid-@mail.com'
+    const email5 = 'invalid_@mail.com'
     const sut = new EmailValidator()
     // arrange
 
     expect(sut.validate(email1)).toBeFalsy()
     expect(sut.validate(email2)).toBeFalsy()
+    expect(sut.validate(email3)).toBeFalsy()
+    expect(sut.validate(email4)).toBeFalsy()
+    expect(sut.validate(email5)).toBeFalsy()
     // arrange
   })
 
   it('should return true when email is valid', () => {
-    const email = 'va.lid-mail_@mail.com'
+    const email = 'va.lid-mail@mail.com'
     const sut = new EmailValidator()
     // arrange
 
