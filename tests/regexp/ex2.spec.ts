9fd26cacc5b41360c77b6c93e9cefdd28b31e23d
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-30 09:09:23 -0300

    feat: ensure RegexVAlidator returns true when validate succeeds

---
+++ tests/regexp/ex2.spec.ts
@@ -0,0 +1,26 @@
+// 2. Write a Python program that matches a string that has an a followed by zero or more b's.
+
+interface Validator {
+  validate(str: string): boolean
+}
+
+class RegexValidator implements Validator{
+  validate(str: string): boolean {
+    const pattern = 'ab+'
+    const regex = new RegExp(pattern, 'gi')
+    return regex.test(str)
+  }
+}
+
+
+describe('RegexValidator', () =>{
+  it('should return true if validation succeeds given a valid string', ()=>{
+    const str = 'abc'
+    const sut = new RegexValidator()
+    // arrange
+
+    const isValidString = sut.validate(str) // act
+
+    expect(isValidString).toBeTruthy() // assert
+  })
+})
