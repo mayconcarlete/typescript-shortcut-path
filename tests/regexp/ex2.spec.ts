2ee05a1664ca5a53a761f8a5a507df1b3e4c0e20
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-05 18:25:35 -0300

    feat: move ex2 to its own file

--- tests/regexp/ex2.spec.ts
+++ tests/regexp/ex2.spec.ts
@@ -1,23 +1,12 @@
 // 2. Write a Python program that matches a string that has an a followed by zero or more b's.
-
-interface Validator {
-  validate(str: string): boolean
-}
-
-class RegexValidator implements Validator{
-  validate(str: string): boolean {
-    const pattern = /a.+/
-    const regex = new RegExp(pattern, 'gi')
-    return regex.test(str)
-  }
-}
+import {Exercise2} from '../../src/regexp/ex2'
 
 
 describe('RegexValidator', () =>{
   it('should return true if validation succeeds given a valid string', ()=>{
     const str1 = 'abc'
     const str2 = 'ac'
-    const sut = new RegexValidator()
+    const sut = new Exercise2()
 
     expect(sut.validate(str1)).toBeTruthy() // assert
     expect(sut.validate(str2)).toBeTruthy() // assert
@@ -26,7 +15,7 @@ describe('RegexValidator', () =>{
 
   it('should return false if validation fails given a invalid string', () => {
     const str = 'dfg'
-    const sut = new RegexValidator()
+    const sut = new Exercise2()
     // arrange
 
     const isValidString = sut.validate(str) // act
