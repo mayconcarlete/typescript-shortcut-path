812566e9ce00bd493a2c208986901dd9431d2eb8
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-08 22:28:42 -0300

    feat: move Exercise6 to its own file

--- tests/regexp/ex6.spec.ts
+++ tests/regexp/ex6.spec.ts
@@ -1,16 +1,7 @@
+import {Exercise6} from '../../src/regexp/ex6'
 // 6. Write a Python program that matches a string that has an a followed by two to three 'b'.
 
-interface Validate {
-  validate(str: string):boolean
-}
 
-class Exercise6 implements Validate {
-  validate(str: string): boolean {
-    const pattern = /ab{2,3}/
-    const regexp = new RegExp(pattern, 'gi')
-    return regexp.test(str)
-  }
-}
 
 describe('Exercise6', () => {
   it('should return true when regexp test returns true', () => {
