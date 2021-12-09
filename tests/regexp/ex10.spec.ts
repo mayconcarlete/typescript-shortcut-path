1deb48c2cdd1edb51944cde9c31746c2965dfdee
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-09 09:12:27 -0300

    feat: ensure Exercise10 returns true when validation succeeds

---
+++ tests/regexp/ex10.spec.ts
@@ -0,0 +1,23 @@
+// 10. Write a Python program that matches a word at the beginning of a string. Go to the editor
+// print(text_match("The quick brown fox jumps over the lazy dog.")) found
+// print(text_match(" The quick brown fox jumps over the lazy dog.")) not found
+
+interface Validate{
+  validate(str:string): boolean
+}
+
+class Exercise10 implements Validate{
+  validate(str: string): boolean {
+    const pattern = /^[a-zA-Z]+/
+    const regex = new RegExp(pattern, 'gi')
+    return regex.test(str)
+  }
+}
+
+describe('Exercise10', () => {
+  it('should return true when validate succeeds', () =>{
+    const sut = new Exercise10()
+
+    expect(sut.validate('The quick brown fox jumps over the lazy dog.')).toBeTruthy()
+  })
+})
