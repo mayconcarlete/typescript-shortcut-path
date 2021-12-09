2a88f2cea01078f6ae0e6abbb317349f01c288bc
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-09 09:14:13 -0300

    feat: move production class to its own file

--- tests/regexp/ex10.spec.ts
+++ tests/regexp/ex10.spec.ts
@@ -1,18 +1,8 @@
+import {Exercise10} from '../../src/regexp/ex10'
 // 10. Write a Python program that matches a word at the beginning of a string. Go to the editor
 // print(text_match("The quick brown fox jumps over the lazy dog.")) found
 // print(text_match(" The quick brown fox jumps over the lazy dog.")) not found
 
-interface Validate{
-  validate(str:string): boolean
-}
-
-class Exercise10 implements Validate{
-  validate(str: string): boolean {
-    const pattern = /^[a-zA-Z]+/
-    const regex = new RegExp(pattern, 'gi')
-    return regex.test(str)
-  }
-}
 
 describe('Exercise10', () => {
   it('should return true when validate succeeds', () =>{
