eceea9a090b8a4ff87ea66c617cd00196f239056
Maycon Carlete <maycon.carlete@gmail.com>
2022-01-26 21:01:57 -0300

    feat: add env on module alias

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,10 @@ import { addAlias } from 'module-alias'
 import { resolve } from 'path'
 import { env } from 'process'
 
-
-addAlias('@', resolve('src'))
 // lembrar que para rodar o ts-node-dev devemos remover a pasta dist,
 // para compilar precisamos adicionar um condicional env.enviroment
+if(process.env.ENV === 'dev'){
+  addAlias('@', resolve('src'))
+}else {
+  addAlias('@', resolve('dist'))
+}
