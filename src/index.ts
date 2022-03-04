9ec2be0886f78fcd295c8658bc2c77ec169a146f
Maycon Carlete <maycon.carlete@gmail.com>
2022-03-04 13:02:56 -0500

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import { Math } from '@/folder1/sub-folder2/sub-folder3/sum'
+import express, {json, Request, Response, NextFunction} from 'express'
 
-const result = Math.sum(2, 2)
-console.log(result)
+const app = express()
+
+app.use(json())
+
+app.get("/", (req: Request, res:Response, next:NextFunction) => {
+  const a = 1
+  if(a===1) return res.status(404).send({error: 'user doesnt exists'})
+  res.status(201).send({status: 'created'})
+})
+
+app.listen(4001, () => {
+  console.log("we are running at 4001 port")
+})
