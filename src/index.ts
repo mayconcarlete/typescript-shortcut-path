7daa7686f2241b4bef98bdc79939eea8d0a6227c
Maycon Carlete <maycon.carlete@gmail.com>
2022-03-04 14:19:12 -0500

    feat: add joi validator to test

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { Math } from '@/folder1/sub-folder2/sub-folder3/sum'
 import express, {json, Request, Response, NextFunction} from 'express'
+const Joi = require("joi")
 
 const app = express()
 
 app.use(json())
 
-app.get("/", (req: Request, res:Response, next:NextFunction) => {
-  const a = 1
-  if(a===1) return res.status(404).send({error: 'user doesnt exists'})
-  res.status(201).send({status: 'created'})
+app.get("/", async(req: Request, res:Response, next:NextFunction) => {
+  try{
+
+    const schema = Joi.object({
+      userId: Joi.string().required(),
+      featureId: Joi.string().required()});
+
+      const validatorResult = await schema.validate(req.query)
+
+      res.status(201).send({status: 'created'})
+    }catch(error: any){
+      if(error.isJoi) return res.status(400).send({error:"Validation Error"})
+    }
 })
 
 app.listen(4001, () => {
