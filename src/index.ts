6a6722fe239c420d23135af290fc446bb68f3605
Maycon Carlete <maycon.carlete@gmail.com>
2022-03-15 10:39:42 -0300

    feat: test next passing error

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,23 @@ const app = express()
 
 app.use(json())
 
+const middleware1 = (req: Request, res: Response, next: NextFunction) => {
+  return new Promise((resolve, reject) => {
+    throw new Error("hello my friend")
+  }).then().catch(error => {
+    next(new Error("I'm passing this error to you my friend..."))
+  })
+}
+
+const middleware2 = (req: Request, res: Response, next: NextFunction) => {
+  return new Promise((resolve, reject) => {
+    console.log("I'm here my friend")
+    next()
+  }).then().catch(error => {
+    console.log("line 20")
+    console.log(error)
+  })
+}
 app.get("/", async(req: Request, res:Response, next:NextFunction) => {
   try{
 
@@ -19,6 +36,11 @@ app.get("/", async(req: Request, res:Response, next:NextFunction) => {
       if(error.isJoi) return res.status(400).send({error: error.details[0].message})
       return res.status(500).send({error: "Server Error"})
     }
+})
+
+app.get("/test", middleware2, middleware1, async(req: Request, res: Response, next: NextFunction) => {
+  console.log("I'm in the route")
+  res.status(200).send({data: "hello from route"})
 })
 
 app.listen(4001, () => {
