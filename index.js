00bd01dd28143a0fad7f0a287667ebb4af54c127
Maycon Carlete <maycon.carlete@gmail.com>
2022-03-22 14:51:52 -0300

    feat: get all url path

--- index.js
+++ index.js
@@ -1,91 +1,26 @@
 
 
-const str = "[com.wsomacp, com.facebook.orca, com.mistplay.mistplay, com.facebook.appmanager, com.microsoft.office.word, com.crazylabs.amaze.game, org.simalliance.openmobileapi.service, com.gd.mobicore.pa, com.amanotes.beathopper, service.odtcfactory.sec.com.odtcfactoryservice, com.osp.app.signin, com.dsi.ant.server, com.youmusic.magictiles, com.dsi.ant.service.socket, com.trustonic.tuiservice, com.microsoft.office.powerpoint, com.skype.raider, com.craftgames.plntcrft, com.microsoft.office.onenote, com.enhance.gameservice, com.dropbox.android, com.facebook.system, com.wssyncmldm, de.axelspringer.yana.zeropage, com.aero.sniperanimalshooting3d, com.microsoft.office.excel, com.facebook.katana, com.skms.android.agent, flipboard.boxer.app, tv.peel.app, com.wssnps, com.msi.logogame, com.microsoft.skydrive, com.mobeam.barcodeService, com.policydm, com.dsi.ant.plugins.antplus, com.dsi.ant.sample.acquirechannels]"
-const str2 = "[com.wsomacp, com.facebook.orca, com.mistplay.mistplay, com.facebook.appmanager, com.microsoft.office.word, com.crazylabs.amaze.game, org.simalliance.openmobileapi.service, com.gd.mobicore.pa, com.amanotes.beathopper, service.odtcfactory.sec.com.odtcfactoryservice, com.osp.app.signin, com.dsi.ant.server, com.youmusic.magictiles, com.dsi.ant.service.socket, com.trustonic.tuiservice, com.microsoft.office.powerpoint, com.skype.raider, com.craftgames.plntcrft, com.microsoft.office.onenote, com.enhance.gameservice, com.dropbox.android, com.facebook.system, com.wssyncmldm, de.axelspringer.yana.zeropage, com.aero.sniperanimalshooting3d, com.microsoft.office.excel, com.facebook.katana, com.skms.android.agent, flipboard.boxer.app, tv.peel.app, com.wssnps, com.msi.logogame, com.microsoft.skydrive, com.mobeam.barcodeService, com.policydm, com.dsi.ant.plugins.antplus, com.dsi.ant.sample.acquirechannels]"
-// str.replace(" ", "").split(",")
-// str[0].split("[", "")
-
-// console.log(str)
-
-// console.log({data: str2.replace("[", "").replace("]","").split(",").map(el => el.trim())})
-const strArrayToArrayStr = (str) => str.replace("[", "").replace("]","").split(",").map(el => el.trim())
-// console.log(strArrayToArrayStr("[samsung_SM-G925F]"))
-// const num = Number("1")
-// console.log(num)
-// console.log("1")
-// const handleNull = strArrayToArrayStr("[null]")
-// console.log("null")
-
-
-// const AWS = require("aws-sdk")
-// const AthenaExpress = require("athena-express")
-// const express = require("express")
 
 
 
-// AWS.config.update(awsCredentials)
+const AWS = require("aws-sdk")
+const AthenaExpress = require("athena-express")
+const express = require("express")
+const app = express()
 
-// const athenaExpressConfig = {aws:AWS, formatJson: true}
-
-// const athenaExpress = new AthenaExpress(athenaExpressConfig)
-
-// let myQuery = "select faceVerified from mistplay_transform.user_fraud_stats where faceVerified is not NULL limit 1"
-// ;
-// (
-//     async() => {
-//         const result = await athenaExpress.query(myQuery)
-//         console.log(result)
-//       }
-//     )()
-
-const user =   {
-  uid: '513d7123-7041-4d0d-84f2-4316dc588432',
-  tue: 676,
-  ltc: 0,
-  ltv: 580,
-  tgp: '4',
-  createdat: '2022-01-25 05:38:23.318',
-  updatedat: '2022-02-25 23:43:41.783',
-  countries: '[CA]',
-  email: 'rylanpatrick99@gmail.com',
-  aips: '[174.90.223.76]',
-  fdu: 'samsung_SM-A520W',
-  ldu: 'samsung_SM-A520W',
-  n_dids: '1',
-  n_bids: '1',
-  n_devices: '1',
-  n_bids_recognized_af: '1',
-  user_media_sources: '[organic]',
-  last_media_source: 'organic',
-  n_users_sharing_ip: '1',
-  n_banned_users_sharing_ip: '0',
-  active_days_span: '31',
-  percent_time_played: 0.24,
-  main_gameplay_hour: '22',
-  level_ups_that_hour: '4',
-  all_breasons: '[-1]',
-  fraud_countries: '[no-br-geos]',
-  sr: '[5]',
-  vesper_score: 0.024662354961037636,
-  vesper_prediction_date: '2022-01-25',
-  ipqs_fraud_score: 100,
-  ipqs_bot_status: 1,
-  ipqs_isabuse: 1,
-  ipqs_isemulateddevice: 1,
-  ipqs_countries: '[CA]',
-  rejected_installs: '0',
-  avg_game_d1_ret: 0,
-  sum_game_d0_time_hour: 1.168911388888889,
-  game_installs_sources: '[organic]',
-  devices: '[samsung_SM-A520W]',
-  installsources: '[com.android.vending]',
-  sim_countries: '[CA]',
-  level_1_apps: '[null]',
-  level_4_apps: '[null]',
-  all_apps: '[com.candywriter.bitlife, com.TwinCrab.Motorpolia, com.kiloo.subwaysurf, com.skms.android.agent, com.microsoft.office.excel, com.trustonic.tuiservice, com.spotify.music, com.disney.disneyplus, com.osp.app.signin, com.sphero.bb8, com.eightsec.Shootero, com.microsoft.office.powerpoint, com.BallGames.Woodturning, com.knox.vpn.proxyhandler, com.nianticlabs.pokemongo, com.bitstrips.imoji, com.policydm, io.voodoo.paper2, com.enhance.gameservice, com.facebook.talk, com.auxbrain.egginc, com.microsoft.skydrive, com.snapchat.android, com.dsi.ant.service.socket, com.sparkistic.photowear, com.hiya.star, com.powerschool.portal, com.yoku.marumovie, com.funny.technology.dimensional, com.wssyncmldm, com.discord, flipboard.boxer.app, com.gd.mobicore.pa, com.time.trigger, com.cateater.stopmotionstudio, com.sem.factoryapp, com.innersloth.spacemafia, io.voodoo.paperio, com.dsi.ant.sample.acquirechannels, com.sonypicturestelevision.qbert, org.simalliance.openmobileapi.service, com.facebook.appmanager, com.facebook.services, com.microsoft.office.word, com.mobeam.barcodeService, com.amazon.mShop.android, com.wsomacp, com.zhiliaoapp.musically, com.stealthgame.master, com.facebook.system, com.Ijnbjh.ghostcleaner, com.roblox.client, com.wssnps, com.facebook.katana, com.dsi.ant.plugins.antplus, com.dsi.ant.server, com.playgendary.tom, com.skype.raider, com.mistplay.mistplay]'
+const awsCredentials = {
+  region: "us-west-2",
 }
 
+const strArrayToArrayStr = (str) => str.replace("[", "").replace("]","").split(",").map(el => el.trim()).filter(el => el !== 'null' )
 const strArrayToArrayStrNumber = (str) => str.replace("[", "").replace("]","").split(",").map(el => el.trim()).map(num => Number(num))
+
+AWS.config.update(awsCredentials)
+
+const athenaExpressConfig = {aws:AWS, formatJson: true}
+
+const athenaExpress = new AthenaExpress(athenaExpressConfig)
+
 
 const parsedUser = (user) => ({
     uid: String(user.uid),
@@ -128,32 +63,15 @@ const parseData = (user) => ({
   ipqs_fraud_score:user.ipqs_fraud_score? Number(user.ipqs_fraud_score): null,
 })
 
-const AWS = require("aws-sdk")
-const AthenaExpress = require("athena-express")
-const express = require("express")
 
-const awsCredentials = {
-  region: "us-west-2",
-}
-
-AWS.config.update(awsCredentials)
-
-const athenaExpressConfig = {aws:AWS, formatJson: true}
-
-const athenaExpress = new AthenaExpress(athenaExpressConfig)
-const app = express()
 
 app.use(express.json())
 app.get("/", async(req, res) =>{
-  console.log("entreei")
-  let myQuery = "select * from mistplay_transform.user_fraud_stats limit 10"
-
-  // let myQuery = "DESCRIBE mistplay_transform.user_fraud_stats"
   try{
-    const result = await athenaExpress.query(myQuery)
-    const data = result.Items.map(data => parseData(data))
+    // const result = await athenaExpress.query(myQuery)
+    // const data = result.Items.map(data => parseData(data))
 
-    res.send(data)
+    res.send({data: `${req.protocol}://${req.get("host")}${req.url}`})
   }catch(error){
     console.log(error)
   }
